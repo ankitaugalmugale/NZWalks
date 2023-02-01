352c815e4dee3292d95d7629bf651ed67a6bfac3
Ugalmugale, Ankita <ankita_ugalmugale@dell.com>
2023-02-01 17:13:13 +0530

    added db configuration

---
+++ NZWalks.API/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext :DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        DbSet<Region> Regions { get; set; }
+        DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+        DbSet<Walk> Walks { get; set; } 
+
+    }
+}
